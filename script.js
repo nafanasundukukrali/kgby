fbe7a13fb1a674c771e2ef8763ba5206616d8f01
nafanasundukukrali <alina.konckina@gmail.com>
2020-05-31 01:39:25 +0300

    full menu list

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
     });
 });
 
-var arr = new Array(10);
+var arr = new Array(50);
 arr[0] = {
     cost: 37, kkal: 206, x_B: 8, Zh: 7.8, x_U: 25.4
 }
@@ -48,6 +48,127 @@ arr[8] = {
 arr[9] = {
     cost: 57, kkal: 336, x_B: 8, Zh: 9.6, x_U: 52.8
 }
+arr[10] = {
+    cost: 171, kkal: 317.6, x_B: 23.4, Zh: 24.8, x_U: 33.6
+}
+arr[11] = {
+    cost: 37, kkal: 130, x_B: 1.7, Zh: 10.3, x_U: 8.2
+}
+arr[12] = {
+    cost: 37, kkal: 102.3, x_B: 1.4, Zh: 5.6, x_U: 7.6
+}
+arr[13] = {
+    cost: 57, kkal: 234, x_B: 7, Zh: 18.8, x_U: 8.8
+}
+arr[14] = {
+    cost: 43, kkal: 236, x_B: 6.5, Zh: 8.0, x_U: 8.4
+}
+arr[15] = {
+    cost: 114, kkal: 200.4, x_B: 4.6, Zh: 8.5, x_U: 3.5
+}
+arr[16] = {
+    cost: 114, kkal: 165, x_B: 15.5, Zh: 5.9, x_U: 12.5
+}
+arr[17] = {
+    cost: 114, kkal: 314, x_B: 15.2, Zh: 24.2, x_U: 8.7
+}
+arr[18] = {
+    cost: 114, kkal: 205.2, x_B: 18.9, Zh: 20.1, x_U: 14.6
+}
+arr[19] = {
+    cost: 74, kkal: 206, x_B: 15.6, Zh: 13.8, x_U: 5.1
+}
+arr[20] = {
+    cost: 74, kkal: 279, x_B: 9.3, Zh: 22.2, x_U: 11.7
+}
+arr[21] = {
+    cost: 57, kkal: 317.6, x_B: 23.4, Zh: 24.8, x_U: 33.6
+}
+arr[22] = {
+    cost: 57, kkal: 234.3, x_B: 7.6, Zh: 7.8, x_U: 14.6
+}
+arr[23] = {
+    cost: 270, kkal: 442, x_B: 38, Zh: 34, x_U: 11.8
+}
+arr[24] = {
+    cost: 114, kkal: 158.3, x_B: 5.1, Zh: 10.6, x_U: 8.7
+}
+arr[25] = {
+    cost: 74, kkal: 264, x_B: 15, Zh: 10.8, x_U: 26.8
+}
+arr[26] = {
+    cost: 74, kkal: 218.3, x_B: 16.1, Zh: 9.5, x_U: 23.2
+}
+arr[27] = {
+    cost: 74, kkal: 300.6, x_B: 18.5, Zh: 4.9, x_U: 44.4
+}
+arr[28] = {
+    cost: 114, kkal: 450, x_B: 19.3, Zh: 18.6, x_U: 45
+}
+arr[29] = {
+    cost: 74, kkal: 370.5, x_B: 30.6, Zh: 12.9, x_U: 34.5
+}
+arr[30] = {
+    cost: 114, kkal: 396, x_B: 19.8, Zh: 25.2, x_U: 21.6
+}
+arr[31] = {
+    cost: 74, kkal: 416, x_B: 14, Zh: 35.9, x_U: 17.8
+}
+arr[32] = {
+    cost: 114, kkal: 276, x_B: 16, Zh: 19.9, x_U: 23.5
+}
+arr[33] = {
+    cost: 114, kkal: 818, x_B: 23, Zh: 54.3, x_U: 58.2
+}
+arr[34] = {
+    cost: 171, kkal: 420, x_B: 16, Zh: 13.9, x_U: 57.9
+}
+arr[35] = {
+    cost: 171, kkal: 385, x_B: 19, Zh: 23.8, x_U: 22.1
+}
+arr[36] = {
+    cost: 270, kkal: 382, x_B: 50.1, Zh: 20.3, x_U: 18.6
+}
+arr[37] = {
+    cost: 270, kkal: 721, x_B: 36, Zh: 14, x_U: 111
+}
+arr[38] = {
+    cost: 350, kkal: 557.1, x_B: 42.5, Zh: 24.4, x_U: 44
+}
+arr[39] = {
+    cost: 114, kkal: 383.1, x_B: 5.2, Zh: 16.3, x_U: 40.2
+}
+arr[40] = {
+    cost: 243, kkal: 264, x_B: 13.4, Zh: 19.7, x_U: 8.4
+}
+
+arr[41] = {
+    cost: 114, kkal: 480.3, x_B: 18.2, Zh: 20.1, x_U: 45.2
+}
+arr[42] = {
+    cost: 74, kkal: 323, x_B: 18, Zh: 14, x_U: 31.1
+}
+arr[43] = {
+    cost: 74, kkal: 570, x_B: 9.3, Zh: 12.4, x_U: 109.1
+}
+arr[44] = {
+    cost: 43, kkal: 102.5, x_B: 0.75, Zh: 0, x_U: 25
+}
+arr[45] = {
+    cost: 57, kkal: 205, x_B: 1.5, Zh: 0, x_U: 50
+}
+arr[46] = {
+    cost: 43, kkal: 82.5, x_B: 0.25, Zh: 0, x_U: 20
+}
+arr[47] = {
+    cost: 57, kkal: 165, x_B: 0.5, Zh: 0, x_U: 40
+}
+arr[48] = {
+    cost: 43, kkal: 102.5, x_B: 0.75, Zh: 0, x_U: 25
+}
+arr[49] = {
+    cost: 57, kkal: 205, x_B: 1.5, Zh: 0, x_U: 50
+}
 
 function getvallues(){
     var sum_cost = 0;
@@ -66,10 +187,10 @@ function getvallues(){
         }
         document.getElementById('Fi').innerHTML = `Итог:`;
         document.getElementById('Sum_cost').innerHTML = `Сумма: ${sum_cost.toFixed(2)} руб`;
-        document.getElementById('Sum_kkal').innerHTML = `Калории: ${sum_kkal.toFixed(0)}`;
-        document.getElementById('Sum_B').innerHTML = `Белки: ${sum_B.toFixed(1)}`;
-        document.getElementById('Sum_Zh').innerHTML = `Жиры: ${sum_Zh.toFixed(1)}`;
-        document.getElementById('Sum_U').innerHTML = `Углеводы: ${sum_U.toFixed(1)}`;
+        document.getElementById('Sum_kkal').innerHTML = `Калории: ${sum_kkal.toFixed(2)}`;
+        document.getElementById('Sum_B').innerHTML = `Белки: ${sum_B.toFixed(2)}`;
+        document.getElementById('Sum_Zh').innerHTML = `Жиры: ${sum_Zh.toFixed(2)}`;
+        document.getElementById('Sum_U').innerHTML = `Углеводы: ${sum_U.toFixed(2)}`;
     }
 }
 
